ef557451003ab64fc4cf5d34181dcff66653fa60
SawatyMittal <sawatymittal93@gmail.com>
2026-05-11 12:10:08 +0530

    changes to upload on vercel

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/auth", authRoutes);
 
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.get("/api/bookings", (req, res) => {
 
  const sql = "SELECT * FROM bookings";
